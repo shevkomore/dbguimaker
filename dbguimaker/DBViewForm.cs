5c6a9864d2cc1299616a46d7399890d0a894b18a
BuildTools <unconfigured@null.spigotmc.org>
2023-01-26 17:51:56 +0200

    added DB view structure

---
+++ dbguimaker/DBViewForm.cs
@@ -0,0 +1,29 @@
+﻿using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dbguimaker
+{
+    public partial class DBViewForm : Form
+    {
+        private static string LIST_VIEW_PATH = "/ViewTemplates/ListView/html/index.html";
+        public ChromiumWebBrowser ChromiumView { get; set; }
+        public DBViewForm()
+        {
+            InitializeComponent();
+            //Initializing ChromiumView
+            string path = Environment.CurrentDirectory + LIST_VIEW_PATH;
+            ChromiumView = new ChromiumWebBrowser(path);
+            ChromiumView.MenuHandler = ChromiumHideMenu.Instance;
+            mainPanel.Controls.Add(ChromiumView);
+            ChromiumView.Dock = DockStyle.Fill;
+        }
+    }
+}
--- dbguimaker/Program.cs
+++ dbguimaker/Program.cs
@@ -16,7 +16,7 @@ namespace dbguimaker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DBViewForm());
         }
     }
 }
